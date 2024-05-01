9c3b9d2d582655b40ae4baf248e35686e961ffc4
Ravinder Singh Negi <83633083+tech-pahadi@users.noreply.github.com>
2024-05-01 19:49:55 +0000

    hello

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 24640
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Dehradoon"
+ 
+// accountEmail "hc@hc.com"
+accountPassword = "212121"
+accountCity = "jaipur"
+
+// console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);

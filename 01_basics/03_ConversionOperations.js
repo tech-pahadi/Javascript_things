146d4780f589a3fd17b7df9fbdd1f35f39ca3022
Ravinder Singh Negi <83633083+tech-pahadi@users.noreply.github.com>
2024-05-01 20:46:27 +0000

    ConversionOperations

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,32 @@
+// let score = "33abc"
+// let score1 = null
+// let score2 = undefined
+// let score3 = "negi"
+// let score4 = true
+
+// console.log(typeof score);
+// console.log(typeof(score));   
+
+
+// let valueInNumber = Number(score)  // number  
+// console.log(typeof(valueInNumber));  // number
+// console.log(valueInNumber);  // Nan
+
+
+/*
+"33" - 33
+"33abc" - NaN
+true - 1;  false - 0;
+*/
+
+// let isLoggedIn = 1 -> true
+// let isLoggedIn =  " "   -> false
+// let isLoggedIn = "negi" -> true
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanisLoggedIn);
+
+/*
+1 - true;  0 - false
+" " - false
+"negi" - true
+*/

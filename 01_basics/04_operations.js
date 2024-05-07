75a9e0fac72d460c121280bef887b1e0d98986cc
Ravinder Singh Negi <83633083+tech-pahadi@users.noreply.github.com>
2024-05-07 20:04:00 +0000

    Operations in js

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,51 @@
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue);     -3
+
+// console.log(2+2);
+// console.log(5-3);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3);
+// console.log(2**2);
+
+
+// tricky things - basics
+
+// let str1 = "Hello"
+// let str2 = " negi"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// Complex situtation
+
+// let value1 = "1" + 2
+// let value2 = "1" + 2
+// let value3 = "1" + 2 + 2
+// let value4 = 1 + 2 + "2"
+
+
+// console.log(typeof value1);
+// console.log(typeof value2);
+// console.log(value3);
+// console.log(value4);
+// console.log( (3+4) * 5 % 3);
+
+
+// console.log(true);
+// console.log(+true);  - 1
+// console.log(true+);  - error
+// console.log(+"");  
+
+
+// let num1,num2,num3
+
+// num1 = num2 = num3 = 2+2
+
+
+// let gameCounter = 100
+// ++gameCounter;   // prefix
+// gameCounter++;   // postfix
+// console.log(gameCounter);

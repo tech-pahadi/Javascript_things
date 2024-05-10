c1cd19fc983826a01afbfab0bf097cce214f06fa
Ravinder Singh Negi <83633083+tech-pahadi@users.noreply.github.com>
2024-05-10 16:13:25 +0000

    datatypes summary of js

---
+++ 01_basics/06_datatypes-summary.js
@@ -0,0 +1,28 @@
+// There are 2 types of data baiscally - primitive and non-primitive (reference type)
+
+// diff. in primitive and non-primitive : call by value and call by reference ka diff.
+
+// how the data store in the memory and how we acess the data so in this basis 2 categorization offically
+
+// Primitive :  7 categories - they all are call by value whenever we copy this from somewhere to somewhere so the orginal data memory refrence not give to us the copy of that data only give to us and the changes happen in the copy only 
+
+// String
+// Number
+// Boolean
+// null
+// undefined // variable declare, memory space declare but what value we insert in it we decide about that
+
+// Symbol - to make some value unique (specially when we do advance frontend in js so in that case we use component and we use the same component in diff. place with slighty changes for ex - button unique so we wrap that button into a symbol )
+
+// BigInt
+
+
+
+
+
+
+// Non-primitive (reference type) - in memory the direct refrence allocated 
+
+// Array
+// Object - for js mastery ( master the object and the browser events that's it)
+// function

bacae5dbbff4f7fdf6344cb7dbd2a72fa7b948c3
Ravinder Singh Negi <83633083+tech-pahadi@users.noreply.github.com>
2024-05-10 08:54:38 +0000

    comparision in js

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,33 @@
+// console.log(2<3);
+// console.log(2>3);
+// console.log(2==3);
+// console.log(2!=3);
+// console.log(2>=3);
+// console.log(2<=3);
+
+
+// console.log("2">1);    // automatically js convert it into 2 that's why we use typescript
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// here the reason is the output is diff. becouse here ( == ) and (<,< >, == , <= work diff.tly)
+// compari. convert null to a number treating it as a 0
+// thats why (3) null>=0 is true and (1) null>0 is false
+
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+
+
+// strict check ===
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+// language inconsistency but hai to hai

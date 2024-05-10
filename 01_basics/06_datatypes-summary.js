8f00e5e6ef29cb397f7495711801676692b2b483
Ravinder Singh Negi <83633083+tech-pahadi@users.noreply.github.com>
2024-05-10 16:32:32 +0000

    datatypes summary

--- 01_basics/06_datatypes-summary.js
+++ 01_basics/06_datatypes-summary.js
@@ -17,7 +17,21 @@
 // BigInt
 
 
+// const score = 100
+// const scoreValue = 100.3 // ( this is also comes in number category)
 
+// const isLoggedIn = false  // boolean
+// const outsideTemp = null   // null 
+// let userEmail;  // undefined
+// let userEmail = undefined // undefined also
+
+// const id = Symbol("123")
+// const anotherId = Symbol("123")
+
+// console.log(id === anotherId);   // symbol work is if even we pass the same value the output/result always comes not same
+
+
+// const bigNumber = 48402384208204420n;  //bigInt (not use as much - scarcely)
 
 
 
@@ -26,3 +40,46 @@
 // Array
 // Object - for js mastery ( master the object and the browser events that's it)
 // function
+
+
+// const heros = ["shaktimaan", "naagraj", "doga"];   // array
+
+// Object
+// let myObj = 
+// {
+//     name : "negi",
+//     age : 21
+// };
+
+
+// functions - we treat a function as a variable (variable way)
+
+// const myFunction = function ()
+// {
+//    console.log("Hello world");
+// };
+
+
+// Js is dynamacially type or static type language ??  - dynamacially as far as i get
+
+// const score = 100 (so we never say to js this is a number so in js we never defined language)
+
+// const score:Number = 100 (in typescript we defined - cosen brother)
+
+
+
+// typeof
+
+const outsideTemp = null 
+console.log(typeof(outsideTemp));  // reason why it is object - becouse of unique it is object
+
+const myFunction = function ()
+{
+   console.log("Hello world");
+};
+
+console.log(typeof(myFunction));  // function return funtion but in actual we called it as a object funtion
+
+const heros = ["shaktimaan", "naagraj", "doga"];  
+
+console.log(typeof(heros));  // object

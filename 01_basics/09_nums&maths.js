2c775b4de8f4020df00401b47c8ba4b7c01509ee
Ravinder Singh Negi <83633083+tech-pahadi@users.noreply.github.com>
2024-05-10 21:53:14 +0000

    nums&maths in js

---
+++ 01_basics/09_nums&maths.js
@@ -0,0 +1,20 @@
+const score = 100
+// console.log(score);
+
+
+const balance = new Number(100)  // nothing happend in output but now we 100% sure the varibale type here is number - it specially show the number in the output and also advant. is we see the properties define also in browser 
+// console.log(balance);
+
+
+// console.log(balance.toString());
+// console.log(typeof balance.toString());
+// also we do this also
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));  // precision value - to fixed we use a lot of times specially when we make ecommerce website so there all the calculations , gst & all the precision value is too long so client not use to that precision too much so thats why we use
+
+
+
+const otherNumber = 23.8966
+console.log(otherNumber.toPrecision(2));
+

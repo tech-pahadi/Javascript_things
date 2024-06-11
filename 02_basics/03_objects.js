c7394635b6053afc05b96b4c3a8a23e36c10acbb
Ravinder Singh Negi <83633083+tech-pahadi@users.noreply.github.com>
2024-06-11 07:34:11 +0000

    objects complete

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -13,10 +13,16 @@
 
 // object literals
 
+const mysymbl = Symbol("Key1")
+
+// so now we use our symbol as a key in our object so lesgoo 
+
 const jsUser  = {
     name : "negi",
     "full name" : "Ravinder singh negi",
     age : 21,
+    // mysymbl: "mykey1",
+    [mysymbl] : "mykey1",
     isLoggedIn : false,
     location : "dehradun",
     email : "negi@xyz.com",
@@ -30,11 +36,63 @@ const jsUser  = {
 
 // acess the objectss
 
-console.log(jsUser.name);   // this is not a good way
+// console.log(jsUser.name);   // this is not a good way
 
 // other way
-console.log(jsUser["email"]);  // becouse bts take it as a string
+// console.log(jsUser["email"]);  // becouse bts take it as a string
 
 // so now if we go to acess the full name so we never ever acess by using .dot method so for that's why we use 2nd method
 
-console.log(jsUser["full name"]); 
+// console.log(jsUser."Full name");  // showing error 
+// console.log(jsUser["full name"]); 
+
+// symbol acess
+// console.log(jsUser.mysymbl);
+// console.log(typeof(jsUser.mysymbl));   // string - so it is not created as a symbol
+
+// console.log(jsUser[mysymbl]);
+// console.log(typeof(jsUser[mysymbl]));
+
+
+// Now how we change our object values
+// jsUser.email = "pahadi@gmail.com"
+// console.log(jsUser["email"]);
+
+// Freeze so by this method we apply freeze in the object and we can't change any value further the changes not propogate we see no error 
+// Object.freeze(jsUser)
+// jsUser.email = "xyz@gmail.com"
+// console.log(jsUser["email"]);
+// console.log(jsUser);
+
+
+// Funtion add in a object 
+
+// in our function there is no discramination we treat as a variable 
+
+jsUser.greeting = function()
+{
+    console.log("Hello js user");
+}
+
+jsUser.greetingTwo = function()
+{
+    console.log(`Hello js user, ${this.name}`);  
+    // `` this are string interpolation
+    // when we refrence same object so we use - this
+}
+
+
+
+// so function return back only comes here only refrence comes of the function
+console.log(jsUser.greeting);
+
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
+
+
+
+
+// more and more times we acess our values using - .
+// but there are some special cases when we acess our value with different method
+

926acda812fc28cb142a912c9fb9fc4e0060f32f
Ravinder Singh Negi <83633083+tech-pahadi@users.noreply.github.com>
2024-07-04 08:40:02 +0000

    array & objects inside functions

--- 03_basics/02_functions.js
+++ 03_basics/02_functions.js
@@ -12,4 +12,45 @@ console.log(calculateCartPricenum1(2));
 console.log(calculateCartPricenum1(200,400,500));  // - only first item comes so in this case we have a rest operator   - so now we take all these values in array and in future we apply loop on array and also add them
 
 
-  
+
+// objects pass in functions and use them
+
+const user =
+{
+    username : 'Ravinder',
+    prices : 199
+};
+
+function handleObject(anyobject)
+{
+    console.log(`User name is ${anyobject.username} & price is ${anyobject.prices}`);
+};
+
+// handleObject(user);
+
+// so typesafety also important in this case that's why people prefer typescript - becouse if in object we type price and inside functions we type prices so in this situation wr check typesafety
+
+
+handleObject(
+{
+    username : 'Jio',
+    prices : 699
+})
+
+
+// pass the arrays inside the functions
+
+
+// const myNewArray = [12,45,32,78]
+
+// here myNewArray is a generic name the thing matter is what we write inside parameter
+
+function returnSecondValue(getArray)
+{
+    // console.log(`The array value is:  ${getArray[3]}`);
+    return getArray[1]
+};
+
+// handleArray1(myNewArray)
+console.log(returnSecondValue([12,45,32,78]));
+

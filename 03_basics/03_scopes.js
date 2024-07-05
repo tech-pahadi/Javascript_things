2a53135bb5f45aa144ac6244d1a5b5343805a69b
Ravinder Singh Negi <83633083+tech-pahadi@users.noreply.github.com>
2024-07-05 04:43:49 +0000

    scopes in js

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -16,6 +16,7 @@
 
 // this is global scope
 var c = 300
+let a = 400
 
 if (true) {
     // this is block scope
@@ -23,11 +24,21 @@ if (true) {
     const b = 20
     var c = 30
     c = 30
+    console.log("Inner: ", a);
 }
 
-// console.log(a)  // - working good
+console.log("Outer ",a)  // - working good
 // console.log(b)  // - working good
 console.log(c)  //  - not good becouse it cross there limit no scope no limit becouse when there is many devloper work on a same project so it creats problem at that time
 
 
 // in global scope whatever we write it avail inside block too but the thing which we write inside block is not going outside
+
+
+
+// what is node and windows scope 
+
+// windows scope  - when we inside our browser in - inspect place so that is when we in our windows scope
+// node scope - when we inside our code enviroment and run the example through node so that is node scope
+
+

286d683fd33b07ced56675adc32294ed5b07cbc1
Ravinder Singh Negi <83633083+tech-pahadi@users.noreply.github.com>
2024-07-15 13:27:19 +0000

    While loop in js

---
+++ 05_basics/02_iterations.js
@@ -0,0 +1,52 @@
+// While loop
+
+let i = 1
+
+while (i<=10) {
+    // console.log(`Value of i is: ${i}`);
+    i = i+2;
+    // i++;
+}
+
+const myArray = [10,20,30,40,50];
+let j = 0
+while (j < myArray.length) {
+    // console.log(`${j} element of array is: ${myArray[j]}`);
+    // j = j+1
+    j++
+}
+
+
+
+
+
+// Do - while - loop. (kaam phle condition check baad mai)
+
+let score = 11
+do {
+    // console.log(`Score is: ${score}`);
+    score++
+} while (score <= 10);
+
+// let score = 11
+// do {
+//     console.log(`Score is: ${score}`);
+//     score++
+// } while (score <= 10);
+
+
+// mera example
+
+
+// let l = 11
+// let score1 = 10
+// do {
+//     console.log(`${l} score is: ${score1}`);
+//     l++
+// } while (l <= score1);
+
+
+
+// now we move on - so to apply loop in array becouse whenver we fetch value form database 10-5 values they always comes in the form of array - same goes with API'S values
+
+// so some specialiazed loops and some direct propeties also for loops available so wee see later all these things
